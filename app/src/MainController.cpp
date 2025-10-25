40f092371072c440f11f898d578eda9314121d13
SofijaJanevska <sofijajan.official@gmail.com>
2025-10-25 18:10:12 +0200

    dodat metod loop(), dodat model ranca

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -1,9 +1,19 @@
 #include "MainController.hpp"
 
+#include <engine/platform/PlatformController.hpp>
+#include <engine/platform/PlatformEventObserver.hpp>
 #include <spdlog/spdlog.h>
 
 namespace app {
     void MainController::initialize() {
         spdlog::info("Main controller initialized");
+    }
+
+    bool MainController::loop() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
+            return false;
+        }
+        return true;
     }
 }
