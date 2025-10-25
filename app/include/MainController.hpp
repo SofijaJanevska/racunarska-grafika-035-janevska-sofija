a338567839a9b78a2303efe80aefeb8732fc565f
SofijaJanevska <sofijajan.official@gmail.com>
2025-10-25 18:57:25 +0200

    dodati metodi update i update_camera

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -11,6 +11,10 @@ namespace app {
 
         void draw_backpack();
 
+        void update_camera();
+
+        void update() override;
+
         void begin_draw() override;
 
         void draw() override;
